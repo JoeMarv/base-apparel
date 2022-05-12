a1b061f487cdf8e2f70314904d203ce5a8ab5de3
Joel <joepotential@gmail.com>
2022-05-12 13:41:57 +0100

    Final

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-document.getElementById('input1').addEventListener('invalid', myFunction);
+document.getElementById('email1').addEventListener('invalid', myFunction);
 var email = document.querySelector('.email1')
 var success = document.querySelector('.success-message')
 var fail = document.querySelector('.invalid-message')
 
 function myFunction() {
-    this.setCustomValidity('');
+    this.setCustomValidity(' ');
 } 
 
 function ValidateEmail(input) {
@@ -12,12 +12,14 @@ function ValidateEmail(input) {
     if (input.value.match(validRegex)) {
         location.reload();
         success.style.display = 'block';
+        fail.style.display = 'none';
         return true;
     } 
     else {
         email1.classList.add('wrong-email');
         fail.style.display = 'block';
+        success.style.display = 'none'
         return false;
     }
     
-}
+};
