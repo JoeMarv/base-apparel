bf55885093b35f4f9311135e01fd153ae89d3e14
Joel <joepotential@gmail.com>
2022-05-12 13:00:20 +0100

    new

---
+++ app.js
@@ -0,0 +1,23 @@
+document.getElementById('input1').addEventListener('invalid', myFunction);
+var email = document.querySelector('.email1')
+var success = document.querySelector('.success-message')
+var fail = document.querySelector('.invalid-message')
+
+function myFunction() {
+    this.setCustomValidity('');
+} 
+
+function ValidateEmail(input) {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9]+)*$/;
+    if (input.value.match(validRegex)) {
+        location.reload();
+        success.style.display = 'block';
+        return true;
+    } 
+    else {
+        email1.classList.add('wrong-email');
+        fail.style.display = 'block';
+        return false;
+    }
+    
+}
